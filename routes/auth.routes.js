6c01b67e1da6a383e8c8e2d11b3e7746314244be
Ting-Chi Wong <tcwong007@gmail.com>
2021-05-20 20:16:59 +0200

    iteration 2 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -35,4 +35,32 @@ router.post("/signup", (req, res, next) => {
 //   })
 //   .catch((err) => next(err));
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other email.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
 module.exports = router;
